4ae2a7b64763b154b56d0a39561a819db270f9be
Justin Lamb <justinlamb@me.com>
2022-02-26 13:11:32 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let add = (a, b) => a + b;
+let subtract = (a, b) => a - b;
+let multiply = (a, b) => a * b;
+let divide = (a, b) => a / b;
+
+
+function operate(operator, a, b) {
+    const mathDict = {'+': add, '-': subtract, '*': multiply, '/': divide};
+    let answer = mathDict[operator](a, b);
+    return answer;
+}
+
