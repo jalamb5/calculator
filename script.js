269584a08066561a8ded8a0130914454bc050a73
Justin Lamb <justinlamb@me.com>
2022-03-01 11:03:54 -0800

    add keyboard support

--- script.js
+++ script.js
@@ -29,62 +29,98 @@ function updateDisplay(userNumber, lastKey, operator) {
     document.getElementById('lastKey').textContent = lastKey;
 };
 
+function updateNum(keyNum) {
+    if (operator === '') {
+        if (keyNum === '.' && userNumber.includes('.')) {
+            keyNum = '';
+        }
+        userNumber += keyNum;
+        updateDisplay(userNumber);
+    }
+    else if (equalFlag === false){ // If the user hasn't clicked '=' yet, they can continue adding to the number
+        if (keyNum === '.' && lastKey.includes('.')) {
+            keyNum = '';
+        }
+        lastKey += keyNum;
+        updateDisplay(userNumber, lastKey, operator);
+    }
+    else { // If the user has clicked '=', we will reset the lastKey once a new number is entered. This prevents lastkey from growing forever.
+        lastKey = keyNum;
+        updateDisplay(userNumber, lastKey, operator);
+        equalFlag = false;
+    }
+}
+
+function setOperator(keyNum) {
+    if (operator !== '') { // evaluate any existing values before updating the operator
+        operate(operator, userNumber, lastKey);
+        operator = '';
+    }
+    operator = keyNum;           
+    updateDisplay(userNumber, lastKey, operator);
+};
+
+function numDel() {
+    if (lastKey > 0) {
+        lastKey = lastKey.slice(0, -1);
+        updateDisplay(userNumber, lastKey, operator);
+    }
+    else if (userNumber > 0 && operator === '') {
+        userNumber = userNumber.slice(0, -1);
+        updateDisplay(userNumber, lastKey, operator);
+        }
+}
+
+function clearDisplay() {
+    userNumber = '';
+    lastKey = '';
+    operator = '';
+    equalFlag = false;
+    updateDisplay(userNumber, lastKey, operator);
+    document.getElementById('error').textContent = "";
+}
+
+
+// Handlers for mouse inputs
 const BUTTONS = document.querySelectorAll('button');
 
 Array.from(BUTTONS).forEach(function(event) {
     event.addEventListener("click", function() {
-        let key = this.textContent;
+        let keyNum = this.textContent;
         if (this.className === 'number') {
-            if (operator === '') {
-                if (key === '.' && userNumber.includes('.')) {
-                    key = '';
-                }
-                userNumber += key;
-                updateDisplay(userNumber);
-            }
-            else if (equalFlag === false){ // If the user hasn't clicked '=' yet, they can continue adding to the number
-                if (key === '.' && lastKey.includes('.')) {
-                    key = '';
-                }
-                lastKey += key;
-                updateDisplay(userNumber, lastKey, operator);
-            }
-            else { // If the user has clicked '=', we will reset the lastKey once a new number is entered. This prevents lastkey from growing forever.
-                lastKey = key;
-                updateDisplay(userNumber, lastKey, operator);
-                equalFlag = false;
-            }
+            updateNum(keyNum);
         }
         else if (this.className === 'operator') {
-            if (operator !== '') { // evaluate any existing values before updating the operator
-                operate(operator, userNumber, lastKey);
-                operator = '';
-            }
-            operator = key;           
-            updateDisplay(userNumber, lastKey, operator);
+            setOperator(keyNum);
         }
-        else if (this.className === 'fn') {
-            if (key === 'Del') {
-                if (lastKey > 0) {
-                    lastKey = lastKey.slice(0, -1);
-                    updateDisplay(userNumber, lastKey, operator);
-                }
-                else if (userNumber > 0 && operator === '') {
-                    userNumber = userNumber.slice(0, -1);
-                    updateDisplay(userNumber, lastKey, operator);
-                }
-            }
+        else if (keyNum === 'Del') {
+            numDel();
         }
         else if (this.id === 'clear') {
-            userNumber = '';
-            lastKey = '';
-            operator = '';
-            equalFlag = false;
-            updateDisplay(userNumber, lastKey, operator);
-            document.getElementById('error').textContent = "";
+            clearDisplay();
         }
         else if (this.id === 'equals') {
             operate(operator, userNumber, lastKey);
         }
     })})
 
+// Handlers for keyboard inputs
+document.addEventListener("keydown", function(event) {
+    keyNum = event.key;
+    let opArray = ['+', '-', '*', '/']
+    if (keyNum >= 0 || keyNum === '.') {
+        updateNum(keyNum);
+    }
+    else if (opArray.includes(keyNum)) {
+        setOperator(keyNum);
+    }
+    else if (keyNum === 'Enter') {
+        operate(operator, userNumber, lastKey);
+    }
+    else if (keyNum === 'c') {
+        clearDisplay();
+    }
+    else if (keyNum === 'Backspace' || keyNum === 'Delete') {
+        numDel();
+    }
+})
